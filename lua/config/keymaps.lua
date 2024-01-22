0ac6b0eb79c943b0bc06e69710fe0b20abaa71df
metsuryuu <qjroman@gmail.com>
2024-01-21 22:30:15 -0500

    adding rust

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,9 @@ keymap({ "t" }, "JK", "<c-\\><c-n>", { desc = "Easier <Esc>", silent = true })
 keymap({ "n", "i", "v" }, "<A-h>", "<cmd>tabprevious<cr>", { desc = "Previous tab", silent = true })
 keymap({ "n", "i", "v" }, "<A-l>", "<cmd>tabnext<cr>", { desc = "Next tab", silent = true })
 keymap({ "v", "n" }, "<leader>y", '\"+y', { desc = "Yank to system clipboard", silent = true })
+
+-- TMUX config
+keymap({ "n" }, "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Tmux Window left", silent = true })
+keymap({ "n" }, "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Tmux Window right", silent = true })
+keymap({ "n" }, "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Tmux Window down", silent = true })
+keymap({ "n" }, "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Tmux Window up", silent = true })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
       "heex",
       "eex",
       "go",
+      "rust"
     },
   },
 }
